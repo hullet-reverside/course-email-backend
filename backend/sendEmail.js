f1cf6419b1dd1cddd21aaec9cad0557b7a8b1f37
hulletmatjiu <hullet.matjiu@reverside.co.za>
2025-10-06 16:14:05 +0200

    Prepare backend for Render deployment

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -13,7 +13,7 @@
  */
 
 const express = require('express');
-const axios = require('axios');
+const axios = require('../node_modules/axios/index.d.cts');
 const cors = require('cors');
 require('dotenv').config();
 
