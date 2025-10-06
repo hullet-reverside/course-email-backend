536ffd4117d7cb9bed9636cc42bf1af5ffe9c26d
hulletmatjiu <hullet.matjiu@reverside.co.za>
2025-10-06 16:52:53 +0200

    Update sendEmail.js

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,7 +1,7 @@
 
 
 const express = require('express');
-const axios = require('../node_modules/axios/index.d.cts');
+const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 
